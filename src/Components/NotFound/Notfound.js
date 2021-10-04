9d988e2e5cb2b2be34656be2c255e75327de086f
Mehedihasan023 <mh20973@gmail.com>
2021-10-04 20:57:27 +0600

    not found edited

--- src/Components/NotFound/Notfound.js
+++ src/Components/NotFound/Notfound.js
@@ -5,7 +5,7 @@ const Notfound = () => {
         <div>
             {/* show some error if link  is  not correct*/}
             <h1>404</h1>
-            <h1>Not Found</h1>
+            <h1>Not Found !!!</h1>
         </div>
     );
 };
